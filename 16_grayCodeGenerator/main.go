aea9f3ae76228a82570b0f797e21e0997fa67d06
Goevex <nicolas.morawietz@googlemail.com>
2017-03-23 00:23:26 +0100

    Finished GrayCode in Golang

--- 16_grayCodeGenerator/main.go
+++ 16_grayCodeGenerator/main.go
@@ -16,7 +16,7 @@ func main() {
 		fmt.Scan(&input)
 		if c, err := strconv.Atoi(input); err == nil {
 			count = c
-			if count > 0 { //strconv.ParseInt() also works
+			if count > 0 {
 				break
 			} else {
 				fmt.Println("Gib eine Zahl größer 0 ein!")
@@ -30,10 +30,15 @@ func main() {
 	}
 
 	fmt.Println("Ausgangs-Array:")
-	var binaryCountArray []string
-	binaryCountArray = append(binaryCountArray, intSliceToBinaryStringSclice(countSlice)...)
+	var binaryCountSlice []string
+	binaryCountSlice = append(binaryCountSlice, intSliceToBinaryStringSclice(countSlice)...)
+	fmt.Println(binaryCountSlice)
+	fmt.Println("")
+	fmt.Println("Gray-Code:")
+	var graySlice []string
+	graySlice = append(graySlice, binaryStringArrayToGrayArray(binaryCountSlice)...)
+	fmt.Println(graySlice)
 
-	fmt.Println(binaryCountArray)
 }
 
 func intSliceToBinaryStringSclice(intSlice []int) []string {
@@ -46,4 +51,25 @@ func intSliceToBinaryStringSclice(intSlice []int) []string {
 		stringArray[i] = fmt.Sprintf(format, stringArray[i])
 	}
 	return stringArray
+}
+
+func binaryStringArrayToGrayArray(stringSlice []string) []string {
+	returnArray := make([]string, len(stringSlice))
+	for i := range stringSlice {
+		returnArray[i] = string(stringSlice[i][0])
+		for j := 1; j < len(stringSlice[i]); j++ {
+			var firstBool, secondBool, resultBool bool
+			var resultPseudoChar string
+			firstBool = (stringSlice[i][j-1] == '1')
+			secondBool = (stringSlice[i][j] == '1')
+			resultBool = (!firstBool && secondBool) || (firstBool && !secondBool) //XOR
+			if resultBool {
+				resultPseudoChar = "1"
+			} else {
+				resultPseudoChar = "0"
+			}
+			returnArray[i] = returnArray[i] + resultPseudoChar
+		}
+	}
+	return returnArray
 }
